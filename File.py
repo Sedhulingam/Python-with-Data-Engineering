99f2f529dfd2826fd74101f2087e29064de64a55
Sedhulingam <sedhulingam123@gmail.com>
2025-08-28 11:47:11 +0530

    Changes made

---
+++ File.py
@@ -0,0 +1,33 @@
+# a= [10,20,30]
+
+# it = iter(a)
+
+# print(next(it)) #print 10
+
+
+# def gen_function(item):
+#     for i in range(2,item+1,2):
+#         yield i
+
+# a = gen_function(10)
+# print(next(a))
+
+# s = open("write1.txt","w")
+# a= input("Enter the input")
+# s.write(a)
+# s.close()
+
+# f = open("write1.txt","r")
+# for i in f:
+#     print(i)
+
+# def outer():
+#     global x
+#     x =10
+#     def inner():
+#         global x
+#         x = x+10
+#         print(x)
+#     inner()
+# outer()
+
