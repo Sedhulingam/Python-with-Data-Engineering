d55b03026722933d238493acf2b6d2de4b409cb9
Sedhulingam <sedhulingam123@gmail.com>
2025-08-29 09:55:23 +0530

    Changes Made and ready to push

---
+++ Temperature.py
@@ -0,0 +1,8 @@
+import TempConversion
+
+x = eval(input("Enter the input in Celcius"))
+
+print("Temperature in Fahrenheit: ",TempConversion.ctof(x))
+
+y = eval(input("Enter the input in Fahrenheit: "))
+print("Temperature in celcius: ",TempConversion.ftoc(y))
